92d549e404345c820732c3a806d94e3c44b38274
Obada Khalili <obada.akhalili@gmail.com>
2021-06-14 19:31:10 +0300

    Setup beautiful-react-diagrams

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import React from "react"
+import { Diagram, createSchema, useSchema } from "beautiful-react-diagrams"
+import "beautiful-react-diagrams/styles.css"
 
-export default function App() {
-  return <h1>Playground</h1>
+const initialSchema = createSchema({
+  nodes: [
+    { id: "node-1", content: "Node 1", coordinates: [250, 60] },
+    { id: "node-2", content: "Node 2", coordinates: [100, 200] },
+    { id: "node-3", content: "Node 3", coordinates: [250, 220] },
+    { id: "node-4", content: "Node 4", coordinates: [400, 200] },
+  ],
+  links: [
+    { input: "node-1", output: "node-2" },
+    { input: "node-1", output: "node-3" },
+    { input: "node-1", output: "node-4" },
+  ],
+})
+
+export default function UncontrolledDiagram() {
+  const [schema, { onChange }] = useSchema(initialSchema)
+
+  return (
+    <div style={{ height: "22.5rem" }}>
+      <Diagram schema={schema} onChange={onChange} />
+    </div>
+  )
 }
